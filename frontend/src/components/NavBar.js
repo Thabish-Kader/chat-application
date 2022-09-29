27c0a64d3ac2f58c6690a5a34adc54d915d81c3a
Thabish <kadertabish2@gmail.com>
2022-09-29 11:58:20 +0400

    login logout feature added

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,15 +1,23 @@
 import '../styles/Navbar.css';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-scroll';
-
-
+import {auth} from '../config/firebase';
+import {useAuthState} from 'react-firebase-hooks/auth'
+import {signOut} from 'firebase/auth'
 
 export const NavBar = () => {
+
+    const [user] = useAuthState(auth);
     const navigate = useNavigate();
 
-    const singUpPage = () => {
-        navigate('/register');
+    const loginPage = () => {
+        navigate('/login');
     }
+
+    const signUserOut = async () => {
+        await signOut(auth);
+        navigate('/login');
+    };
     return (
         <>
         <nav className='navbar'>
@@ -21,8 +29,18 @@ export const NavBar = () => {
                     <Link to="services-section" spy={true} smooth={true} offset={-70} duration={500} id='nav-link'>Services</Link>
                     <Link to='issue' spy={true} smooth={true} offset={-70} duration={500} id='nav-link'>Issue</Link>
                 </div>
-
-                <button className='glow-on-hover' onClick={singUpPage}>Sign Up</button>
+                <div className='user-info'>
+                    {user ? 
+                    <>
+                    <p className='user-text'>Signed in as <span>{user?.displayName}</span></p>
+                    <button className='glow-on-hover' onClick={signUserOut}>Log Out</button>
+                    </>
+                    : 
+                    <>
+                    <button className='glow-on-hover' onClick={loginPage}>Log In</button>
+                    </>    
+                    }
+                </div>
             </div>
     </nav>
     </>
