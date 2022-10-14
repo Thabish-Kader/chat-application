8d26bc9c544607c8358b4bf97c6557a6285e3514
Thabish <kadertabish2@gmail.com>
2022-10-14 11:02:58 +0400

    final

--- frontend/src/Pages/IssuesList.js
+++ frontend/src/Pages/IssuesList.js
@@ -1,62 +1,62 @@
-import React, {useState, useEffect} from 'react'
-import '../styles/IssuesList.css'
-import  {SingleIssue}  from '../components/SingleIssue'
-import {getDocs, collection} from 'firebase/firestore'
-import axios from 'axios';
-import {db} from '../config/firebase';
+import React, { useState, useEffect } from "react";
+import "../styles/issuesList.css";
+import { SingleIssue } from "../components/SingleIssue";
+import { getDocs, collection } from "firebase/firestore";
+import axios from "axios";
+import { db } from "../config/firebase";
 
 export const IssuesList = () => {
-    const issuesRef = collection(db,'issues');
-    const [issuesList, setIssuesList] = useState([]);
+	const issuesRef = collection(db, "issues");
+	const [issuesList, setIssuesList] = useState([]);
 
-    const [search, setSearch] = useState('');
+	const [search, setSearch] = useState("");
 
-    const getIssues = async () => {
-        const data = await getDocs(issuesRef);
-        setIssuesList((data.docs.map((doc) => ({...doc.data(),id: doc.id}))));
-    };
+	const getIssues = async () => {
+		const data = await getDocs(issuesRef);
+		setIssuesList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+	};
 
-useEffect(() => {
-    getIssues();
+	useEffect(() => {
+		getIssues();
+	}, [issuesList]);
 
-}, [issuesList]);
-    
+	const handleChange = (e) => {
+		setSearch(e.target.value);
+	};
 
-const handleChange = e => {
-    setSearch(e.target.value);
+	const filterSearch = issuesList.filter((data) =>
+		data.Title.toLowerCase().includes(search.toLowerCase())
+	);
+
+	return (
+		<div className="issueList-container">
+			<div className="search-component">
+				<h1 className="issue-text">Search Issues</h1>
+				<form>
+					<input
+						type="text"
+						className="search-issue"
+						placeholder="Search"
+						onChange={handleChange}
+					/>
+				</form>
+			</div>
+			<div className="singleIssue-container">
+				<div className="issueList-row">
+					<div className="user">
+						<h1 className="username-text">Username</h1>
+					</div>
+					<div className="issue-data">
+						<p className="issue-title">Title</p>
+						<p className="issue-description">Description</p>
+						<p className="issue-priority">Priority</p>
+					</div>
+					<div className="issueList-btns"></div>
+				</div>
+			</div>
+			{filterSearch?.map((data) => (
+				<SingleIssue key={data.id} data={data} />
+			))}
+		</div>
+	);
 };
-
-const filterSearch = issuesList.filter(data => 
-    data.Title.toLowerCase().includes(search.toLowerCase())
-    );
-
-
-
-    return (
-        <div className='issueList-container'>
-            <div className='search-component'>
-                <h1 className='issue-text'>Search Issues</h1>
-                <form>
-                    <input type='text' className='search-issue' placeholder='Search' onChange={handleChange}/>
-                </form>
-            </div>
-            <div className='singleIssue-container'>
-                <div className='issueList-row'>
-                <div className='user'>
-                    <h1 className='username-text'>Username</h1>
-                </div>
-                <div className='issue-data'>
-                    <p className='issue-title' >Title</p>
-                    <p className='issue-description'>Description</p>
-                    <p className='issue-priority'>Priority</p>
-                </div>
-                <div className='issueList-btns'>
-                </div>
-                </div>
-        </div>
-            {filterSearch?.map((data) => (
-                <SingleIssue key={data.id} data={data}/>
-            ))}
-    </div>
-    )
-}
--- frontend/src/components/SingleIssue.js
+++ frontend/src/components/SingleIssue.js
@@ -1,48 +1,67 @@
-import React from 'react';
-import '../styles/IssuesList.css'
+import React from "react";
+import "../styles/issuesList.css";
 import { doc, deleteDoc } from "firebase/firestore";
-import {db } from '../config/firebase';
-import {useNavigate} from 'react-router-dom'
+import { db } from "../config/firebase";
+import { useNavigate } from "react-router-dom";
 
-export const SingleIssue = ({data}) => {
-    const navigate = useNavigate()
+export const SingleIssue = ({ data }) => {
+	const navigate = useNavigate();
 
-    const {Username, Title, Description, Priority} = data;
+	const { Username, Title, Description, Priority } = data;
 
+	const resolveIssue = async (id) => {
+		const deleteIssue = doc(db, "issues", id);
+		await deleteDoc(deleteIssue);
+	};
 
-    const resolveIssue =  async (id) => {
-        const deleteIssue = doc(db,'issues', id);
-        await deleteDoc(deleteIssue);
-    }
-    
-    const navigateChat = () => {
-        navigate('/chat')
-    }
+	const navigateChat = () => {
+		navigate("/chat");
+	};
 
-    return (
-        <div className='singleIssue-container'>
-                <div className='issueList-row'>
-                <div className='user'>
-                    <h1 className='username-text'>{Username}</h1>
-                </div>
-                <div className='issue-data'>
-                    <p className='issue-title'>{Title}</p>
-                    <p className='issue-description'>{Description}</p>
+	return (
+		<div className="singleIssue-container">
+			<div className="issueList-row">
+				<div className="user">
+					<h1 className="username-text">{Username}</h1>
+				</div>
+				<div className="issue-data">
+					<p className="issue-title">{Title}</p>
+					<p className="issue-description">{Description}</p>
 
-                    {Priority === 'High' && <p className='issue-priority' style={{color:"red"}}>{Priority}</p>}
-                    {Priority === 'Low' && <p className='issue-priority' style={{color:"green"}}>{Priority}</p>}
-                    {Priority === 'Medium' && <p className='issue-priority' style={{color:"yellow"}}>{Priority}</p>}
-                    
-
-                    
-                    
-                </div>
-                <div className='issueList-btns'>
-                    <button id='resolve-btn' onClick={() => resolveIssue(data.id)}>Resolve</button>
-                    <button id='discuss-btn' onClick={navigateChat}>Discuss</button>
-                </div>
-                </div>
-        </div>
-    )
-}
-
+					{Priority === "High" && (
+						<p className="issue-priority" style={{ color: "red" }}>
+							{Priority}
+						</p>
+					)}
+					{Priority === "Low" && (
+						<p
+							className="issue-priority"
+							style={{ color: "green" }}
+						>
+							{Priority}
+						</p>
+					)}
+					{Priority === "Medium" && (
+						<p
+							className="issue-priority"
+							style={{ color: "yellow" }}
+						>
+							{Priority}
+						</p>
+					)}
+				</div>
+				<div className="issueList-btns">
+					<button
+						id="resolve-btn"
+						onClick={() => resolveIssue(data.id)}
+					>
+						Resolve
+					</button>
+					<button id="discuss-btn" onClick={navigateChat}>
+						Discuss
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
