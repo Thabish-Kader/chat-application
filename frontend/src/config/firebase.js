20bc3645a500b4f239f7f42d8e83b07ffe3a0dde
Thabish <kadertabish2@gmail.com>
2022-10-14 11:07:12 +0400

    final config

--- frontend/src/config/firebase.js
+++ frontend/src/config/firebase.js
@@ -1,32 +1,23 @@
 // Import the functions you need from the SDKs you need
 
 import { initializeApp } from "firebase/app";
-import {getAuth, GoogleAuthProvider} from 'firebase/auth'
-import {getFirestore} from 'firebase/firestore';
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
-
-    apiKey: "AIzaSyB06GLjFlGdiStYqNs7XKiTz_tQqECweZE",
-
-    authDomain: "react-webapp-7defc.firebaseapp.com",
-
-    projectId: "react-webapp-7defc",
-
-    storageBucket: "react-webapp-7defc.appspot.com",
-
-    messagingSenderId: "571149835130",
-
-    appId: "1:571149835130:web:624431ec2fa85dee9914c3"
-
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER,
+	appId: process.env.REACT_APP_APP_ID,
 };
-
 
 // Initialize Firebase
 
